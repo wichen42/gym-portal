0cd77bc3b5074bfed236a461ea35c336752b944b
Wilson Chen <wilsonchen901@gmail.com>
2024-02-29 13:39:16 -0500

    add button and links

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -10,6 +10,10 @@ return (
             <h1>YOUR ASSIGNMENT IS FITNESS - </h1>
             <h1 id='sub-landing-text'>JOIN THE MOVEMENT</h1>
             <p>PRJCT MVMT is a boutique fitness studio focused on making you feel, look, and MOVE better! Join us now to make fitness part of your everyday lifestyle whether it's one on one training or our small group training, we’ll get you moving.</p>
+            <div className="button-container">
+                <a className="button" href="https://www.vagaro.com/projectmovementstudio/services" target='_blank'>Join Now</a>
+                <span>Learn More</span>
+            </div>
         </div>
         {/* IMAGE */}
         <div className="landing-image">
