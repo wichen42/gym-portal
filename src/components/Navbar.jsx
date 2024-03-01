399ea98fdf6343fe7d18f00caeb786777b7475ab
Wilson Chen <wilsonchen901@gmail.com>
2024-03-01 12:59:57 -0500

    complete desktop navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
-
+import logo_black from '../assets/LogoBlack.png';
+import logo_gold from '../assets/LogoGold.png';
 
 const Navbar = () => {
 
     return (
         <div className="navbar-container">
-            hello
+            <div className="logo-container">
+                <img src={logo_gold} alt="logo gold" />
+            </div>
+            <div className="navlinks-container">
+                <a href="">HOME</a>
+                <a href="">PROGRAMS</a>
+                <a href="">TESTIMONIALS</a>
+                <a href="">CONTACT</a>
+            </div>
+            <div className="booking-container">
+            <a className="book-button" href="https://www.vagaro.com/projectmovementstudio/services" target='_blank'>GET STARTED</a>
+            </div>
         </div>
     )
 }
