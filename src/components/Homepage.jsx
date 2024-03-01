b26c74fc178962a85927219df639a7e11682bd42
Wilson Chen <wilsonchen901@gmail.com>
2024-03-01 15:49:16 -0500

    add react icons and program text

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,13 @@
 import Landing from "./Landing"
 import Navbar from "./Navbar"
+import Programs from "./Programs"
 
 const Homepage = () => {
     return (
         <div className="homepage-container">
             <Navbar />
             <Landing />
+            <Programs />
         </div>
     )
 }
--- src/components/Programs.jsx
+++ src/components/Programs.jsx
@@ -1,7 +1,51 @@
+import Program from "./Program"
+import { FaUserAlt, FaUsers} from "react-icons/fa";
+import { AiOutlineLaptop } from "react-icons/ai";
+import { IconContext } from 'react-icons';
 
 const Programs = () => {
+    let online = {
+        title: "Online Training",
+        desc: "Customized programming and nutritional guidance for motivated individuals. With 24/7 access to our coaches and weekly check-ins all done on an app; we offer a premium coaching experience.",
+    }
+    let personal = {
+        title: "Personal Training",
+        desc: "One on one individualized training focused on helping our clients achieve their fitness goals. With undivided attention to our clients we help program, teach, and coach our clients so they leave with more than just a workout.",
+    }
+    let group = {
+        title: "Group Training",
+        desc: "Our small group classes are focused on delivering heart pumping and fat shedding results. In our Strength & Conditioning based HIIT classes we will generate POWERFUL movements to get you moving.",
+    }
     return (
-        <div>Programs</div>
+        <div className="programs-container">
+            <h1>MORE THAN JUST A GYM</h1>
+            <div className="programs">
+                <Program 
+                    title={online.title} 
+                    desc={online.desc}
+                    icon={
+                        <IconContext.Provider value={{size: "25px"}}>
+                            <AiOutlineLaptop />
+                        </IconContext.Provider>
+                    }/>
+                <Program 
+                    title={personal.title} 
+                    desc={personal.desc}/>
+                    icon={
+                        <IconContext.Provider value={{size: "25px"}}>
+                            <FaUserAlt />
+                        </IconContext.Provider>
+                    }
+                <Program 
+                    title={group.title} 
+                    desc={group.desc}
+                    icon={
+                        <IconContext.Provider value={{size: "25px"}}>
+                            <FaUsers />
+                        </IconContext.Provider>
+                    }/>
+            </div>
+        </div>
     )
 }
 
